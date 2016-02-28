55d58af1795ba0fbcba235b48d48484586c4752f
MTTHWBSH <bushmat4@gmail.com>
2016-02-27 19:18:05 -0600

    some keyboard avoidance shit

--- Gaggle/EditPostViewController.swift
+++ Gaggle/EditPostViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class EditPostViewController: ViewController, UITextFieldDelegate {
+class EditPostViewController: ViewController, UITextFieldDelegate, UIScrollViewDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var submitButton: PrimaryButton!
     @IBOutlet var maskView: UIView!
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var subtitleTextField: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
     
     var image: UIImage?
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
@@ -26,6 +32,9 @@ class EditPostViewController: ViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: nil)
     }
     
     func setup() {
@@ -38,6 +47,8 @@ class EditPostViewController: ViewController, UITextFieldDelegate {
         titleTextField.autocorrectionType = .No
         subtitleTextField.autocapitalizationType = .None
         subtitleTextField.autocorrectionType = .No
+        
+        scrollView.delegate = self
         
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
         view.addGestureRecognizer(tap)
@@ -80,6 +91,31 @@ class EditPostViewController: ViewController, UITextFieldDelegate {
             SVProgressHUD.showErrorWithStatus(alertText, maskType: SVProgressHUDMaskType.Black)
         }
     }
+    
+    // MARK: Keyboard Avoidance
+    
+    func keyboardWillShow(note: NSNotification) {
+        let keyboardFrameEnd: CGRect = (note.userInfo![UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+        var scrollViewContentSize: CGSize = scrollView.bounds.size
+        scrollViewContentSize.height += keyboardFrameEnd.size.height
+        scrollView.contentSize = scrollViewContentSize
+        
+        var scrollViewContentOffset: CGPoint = scrollView.contentOffset
+        scrollViewContentOffset.y = scrollViewContentOffset.y + keyboardFrameEnd.size.height*3.0 - UIScreen.mainScreen().bounds.size.height
+        
+        scrollView.setContentOffset(scrollViewContentOffset, animated: true)
+    }
+    
+    func keyboardWillHide(note: NSNotification) {
+        let keyboardFrameEnd: CGRect = (note.userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        var scrollViewContentSize: CGSize = scrollView.bounds.size
+        scrollViewContentSize.height -= keyboardFrameEnd.size.height
+        UIView.animateWithDuration(0.200, animations: {
+            self.scrollView.contentSize = scrollViewContentSize
+        })
+    }
+    
+    // MARK: IBActions
     
     @IBAction func submitButtonPressed(sender: AnyObject) {
         submitPost()
